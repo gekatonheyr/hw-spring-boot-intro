481b0bfd2e140fb8c9a63f860b6144db550cef10
Geks <gekatonheyr@ukr.net>
2025-04-09 12:33:37 +0300

    Had configured initial properties for hw-spring-boot-intro project.

---
+++ src/main/java/mate/academy/hwspringbootintro/HwSpringBootIntroApplication.java
@@ -0,0 +1,13 @@
+package mate.academy.hwspringbootintro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HwSpringBootIntroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HwSpringBootIntroApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mate/academy/hwspringbootintro/HwSpringBootIntroApplicationTests.java
@@ -0,0 +1,13 @@
+package mate.academy.hwspringbootintro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HwSpringBootIntroApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
