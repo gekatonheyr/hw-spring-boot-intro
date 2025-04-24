9eac4b1828d036b9452efc5e7047f3c443ddebdb
Geks <gekatonheyr@ukr.net>
2025-04-24 21:29:29 +0300

    Had worked under the homework: - added paging and sorting to Bookstore project; - added openAPI documentation with corresponding tags and operation annotation, with description.

--- src/main/java/mate/academy/hwspringbootintro/controller/BookController.java
+++ src/main/java/mate/academy/hwspringbootintro/controller/BookController.java
@@ -1,12 +1,15 @@
 package mate.academy.hwspringbootintro.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
-import java.util.List;
 import lombok.RequiredArgsConstructor;
 import mate.academy.hwspringbootintro.dto.BookDto;
 import mate.academy.hwspringbootintro.dto.BookSearchParameters;
 import mate.academy.hwspringbootintro.dto.CreateBookRequestDto;
 import mate.academy.hwspringbootintro.service.BookService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,41 +21,63 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "Bookstore API documentation", description = "Bookstore management endpoint API "
+        + "description.")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/books")
 public class BookController {
     private final BookService bookService;
 
+    @Operation(summary = "Get all books catalog", description = "Use this endpoint to get all "
+            + "books data. Endpoint supports pagination and sorting by using GET method path"
+            + "parameters 'page', 'size' and 'sort")
     @GetMapping
-    public List<BookDto> getAll() {
-        return bookService.findAll();
+    public Page<BookDto> getAll(Pageable pageable) {
+        return bookService.findAll(pageable);
     }
 
+    @Operation(summary = "Get book by id", description = "This endpoint gives all the data about"
+            + " the book with given id.")
     @GetMapping("/{id}")
     public BookDto getBookById(@PathVariable Long id) {
         return bookService.getBookById(id);
     }
 
+    @Operation(summary = "Create new book.", description = "This endpoint allows to create new "
+            + "book record by using JSON body to carry the data about the book object. See the "
+            + "example to give right format of the request.")
     @PostMapping
     public BookDto createBook(@RequestBody @Valid CreateBookRequestDto bookDto) {
         return bookService.save(bookDto);
     }
 
+    @Operation(summary = "Update the book record.", description = "Use this endpoint to update "
+            + "the book data. You can update the book data with ID given as path variable and "
+            + "putting the needed data to the JSON body f the request. See example to put the "
+            + "right data to the request to avoid of errors.")
     @PutMapping("/{id}")
     public BookDto updateBook(@PathVariable Long id,
                               @RequestBody @Valid CreateBookRequestDto bookDto) {
         return bookService.updateBook(id, bookDto);
     }
 
+    @Operation(summary = "Delete book.", description = "This endpoint allows you to delete the "
+            + "book record by giving book ID as the path variable. If such ID is absent in DB "
+            + "you will be responded by exception message, otherwise if operation succeed "
+            + "NO_CONTENT status will be sent.")
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteBook(@PathVariable Long id) {
         bookService.deleteBook(id);
     }
 
+    @Operation(summary = "Search the books DB by parameters", description = "This endpoint "
+            + "allows you to search through the books DB and find books by giving such "
+            + "parameters as 'author', 'title' and/or 'isbn'. By the way 'title' parameter could"
+            + "be given partially, other params need to strictly match the data in the DB.")
     @GetMapping("/search")
-    public List<BookDto> search(BookSearchParameters requestParams) {
-        return bookService.search(requestParams);
+    public Page<BookDto> search(BookSearchParameters requestParams, Pageable pageable) {
+        return bookService.search(requestParams, pageable);
     }
 }
--- src/main/java/mate/academy/hwspringbootintro/service/BookService.java
+++ src/main/java/mate/academy/hwspringbootintro/service/BookService.java
@@ -1,14 +1,15 @@
 package mate.academy.hwspringbootintro.service;
 
-import java.util.List;
 import mate.academy.hwspringbootintro.dto.BookDto;
 import mate.academy.hwspringbootintro.dto.BookSearchParameters;
 import mate.academy.hwspringbootintro.dto.CreateBookRequestDto;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface BookService {
     BookDto save(CreateBookRequestDto book);
 
-    List<BookDto> findAll();
+    Page<BookDto> findAll(Pageable pageable);
 
     BookDto getBookById(Long id);
 
@@ -16,5 +17,5 @@ public interface BookService {
 
     void deleteBook(Long id);
 
-    List<BookDto> search(BookSearchParameters params);
+    Page<BookDto> search(BookSearchParameters params, Pageable pageable);
 }
