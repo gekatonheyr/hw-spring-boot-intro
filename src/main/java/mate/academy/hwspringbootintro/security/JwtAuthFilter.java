4a3b14663eb926e220192e87cd313269195055b7
Geks <gekatonheyr@ukr.net>
2025-05-06 20:46:57 +0300

    UNDERSTOOD THE IDEA!!!

--- src/main/java/mate/academy/hwspringbootintro/security/JwtAuthFilter.java
+++ src/main/java/mate/academy/hwspringbootintro/security/JwtAuthFilter.java
@@ -18,7 +18,7 @@ import org.springframework.web.filter.OncePerRequestFilter;
 @Component
 @RequiredArgsConstructor
 public class JwtAuthFilter extends OncePerRequestFilter {
-    private static final String TOKEN_HEADER = "Bearer";
+    private static final String TOKEN_HEADER = "Bearer ";
 
     private final JwtUtils jwtUtils;
     private final UserDetailsService userDetailsService;
